c28c1e7e5af4d14bf52a44b50209d5eafa4d0bc1
Max Schumacher <maximilianbschumacher@gmail.com>
2025-04-26 12:59:35 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ export default {
 		// introduce Hono?
 		const url = new URL(request.url);
 		switch (url.pathname) {
-			case '/api/pops_with_status':
-				if (request.method !== 'GET') {
-					return errorResponse(`This endpoint only supports GET requests, you sent a ${request.method} request.`, 405);
-				}
-				return Response.json(await getPopsWithStatus(request, env, ctx));
-			case '/api/pops_with_coordinates':
-				if (request.method !== 'GET') {
-					return errorResponse(`This endpoint only supports GET requests, you sent a ${request.method} request.`, 405);
-				}
-				return Response.json(await getPopCoordinates(request, env, ctx));
 			case '/api/pops_with_coordinates_and_status':
 				if (request.method !== 'GET') {
 					return errorResponse(`This endpoint only supports GET requests, you sent a ${request.method} request.`, 405);
